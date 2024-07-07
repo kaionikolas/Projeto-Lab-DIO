8fbb9976576b2bd9302cd3f49416dfdd26280c8a
kaionikolas <kaionikolass@gmail.com>
2024-07-07 17:47:23 -0300

    commit projeto 5 - lab DIO: FastAPI

---
+++ FastAPI/workout_API/categorias/schemas.py
@@ -0,0 +1,11 @@
+from typing import Annotated
+from pydantic import UUID4, Field
+from workout_API.contrib.models import BaseSchema
+
+
+class CategoriaIn(BaseSchema):
+    nome: Annotated[str, Field(description='Nome da categoria', example='Scale', max_length=10)]
+
+
+class CategoriaOut(CategoriaIn):
+    id: Annotated[UUID4, Field(description='Identificador da Categoria')]
---
+++ FastAPI/workout_API/contrib/repository/models.py
@@ -0,0 +1,3 @@
+from workout_API.categorias.models import CategoriaModel
+from workout_API.atleta.models import AtletaModel
+from workout_API.centro_treinamento.models import CentroTreinamentoModel
